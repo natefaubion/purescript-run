89c707fa25a0036aa45120c8ae40e70a4d4ad250
Dustin Whitney <dustin.whitney@gmail.com>
2017-10-12 23:49:07 -0400

    got rid of the algebra and using just straight maybe

--- src/Run/Maybe.purs
+++ src/Run/Maybe.purs
@@ -8,33 +8,21 @@ import Data.Maybe (Maybe(..))
 import Run (Run, lift, on, send)
 import Run as Run
 
-
-data MaybeEffect a
-  = JustEffect a
-  | NothingEffect
-derive instance maybeEffectInstance :: Functor MaybeEffect
-
-type MAYBE = FProxy MaybeEffect
+type MAYBE = FProxy Maybe
 
 _maybe :: SProxy "maybe"
 _maybe = SProxy
 
 liftMaybe :: forall a r. Maybe a -> Run(maybe :: MAYBE | r) a
-liftMaybe (Just a) = lift _maybe (JustEffect a)
-liftMaybe Nothing = lift _maybe NothingEffect
+liftMaybe = lift _maybe 
 
-handleMaybe :: forall a r. MaybeEffect a -> (Run r (Maybe a))
-handleMaybe (JustEffect a) = pure $ Just a
-handleMaybe NothingEffect  = pure Nothing 
-
--- runPure :: forall r1 r2 a. (VariantF r1 (Run r1 a) -> Step (Run r1 a) (VariantF r2 (Run r1 a))) -> Run r1 a -> Run r2 a
 runMaybe :: forall a r. Run (maybe :: MAYBE | r) a -> Run r (Maybe a)
 runMaybe = loop
   where
-    handle = on _maybe Left Right
-    loop r = case Run.peel r of
-      Left a -> case handle a of
-        Left NothingEffect -> pure Nothing
-        Left (JustEffect b) -> loop b
-        Right a' -> send a' >>= runMaybe
-      Right a -> pure $ Just a
+  handle = on _maybe Left Right
+  loop r = case Run.peel r of
+    Left a -> case handle a of
+      Left Nothing -> pure Nothing
+      Left (Just b) -> loop b
+      Right a' -> send a' >>= runMaybe
+    Right a -> pure $ Just a
